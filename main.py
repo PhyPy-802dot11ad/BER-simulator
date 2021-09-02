688e31484e7886d3a0da1a7f10b9dd16042fbbf7
AlexanderMarinsek <alexander.marinsek@gmail.com>
2021-09-02 09:48:18 +0200

    Included separete max_allowed_iteration selection for every MCS.

--- main.py
+++ main.py
@@ -7,21 +7,25 @@ from Simulation import simulate_RX_loop, run_simulation_RX_loop
 from Log import LogIndividualMCSAndEbNo
 
 
+os.nice(10) # Set niceness so other can work on the machine
+
 SUBPROCESS_POOL_SIZE = 4 # Determines how often early stopping will be checked, in terms of EbNo batch simulations.
 
 MIN_ERROR_BITS = 100 # As in MATLAB docs
-MAX_TRANSMITTED_BITS = 10**8 # As in MATLAB docs
+MAX_TRANSMITTED_BITS = 10**9 # As in MATLAB docs
 MIN_SENT_PACKETS = 3 # Custom to smooth out parts of the curve with high BER values
 
 # MIN_ERROR_BITS = 0 # As in MATLAB docs
 # MAX_TRANSMITTED_BITS = 10**8 # As in MATLAB docs
 # MIN_SENT_PACKETS = 1 # Custom to smooth out parts of the curve with high BER values
 
-Eb_N0_db_list = np.arange(0.0, 15.0, 0.5)
+# Eb_N0_db_list = np.arange(0.0, 15.0, 0.25)
+Eb_N0_db_list = np.arange(0.0, 1.0, 0.25)
 
 MCS_list = np.array([2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 12.1, 12.3, 12.4, 12.5])
 demapping_algorithm = 'decision threshold'
-max_decoder_iterations = 10
+# max_decoder_iterations = 10
+max_decoder_iterations = [20, 18, 17, 20, 10, 9, 8, 10, 5, 4, 4, 5, 3, 3, 3] # Corresponding to MCS, just before decoder becomes bottleneck
 
 decoding_algorithm = 'MSA'
 allow_early_exit = True
@@ -61,7 +65,7 @@ fun = lambda MCS, demapping_algorithm, max_decoder_iterations: run_simulation_RX
 )
 
 number_of_combinations = MCS_list.size * Eb_N0_db_list.size
-print(f'Running {number_of_combinations} sub-processes from a pool with size {MCS_list.size}*{SUBPROCESS_POOL_SIZE}.')
+print(f'Running {number_of_combinations} sub-processes from a pool with size {MCS_list.size*SUBPROCESS_POOL_SIZE}.')
 
 # for MCS in MCS_list:
 #     for Eb_N0_db in Eb_N0_db_list:
@@ -69,7 +73,10 @@ print(f'Running {number_of_combinations} sub-processes from a pool with size {MC
 #         p.start()
 #         # fun(MCS, Eb_N0_db, demapping_algorithm, max_decoder_iterations)
 
-for MCS in MCS_list:
-    p = mp.Process(target=fun, args=(MCS, demapping_algorithm, max_decoder_iterations))
+# for MCS in MCS_list:
+for MCS, mdi in zip(MCS_list, max_decoder_iterations):
+    # p = mp.Process(target=fun, args=(MCS, demapping_algorithm, max_decoder_iterations))
+    p = mp.Process(target=fun, args=(MCS, demapping_algorithm, mdi))
     p.start()
     # fun(MCS, demapping_algorithm, max_decoder_iterations)
+    # fun(MCS, demapping_algorithm, mdi)
